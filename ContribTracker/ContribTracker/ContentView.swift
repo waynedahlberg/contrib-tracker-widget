1d30e313b19a316fa9de8fa60563c59af3be829e
Wayne Dahlberg <waynefj40@gmail.com>
2024-03-10 14:32:21 -0600

    Initial commit

---
+++ ContribTracker/ContribTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContribTracker
+//
+//  Created by Wayne Dahlberg on 3/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, wayne!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ ContribTracker/ContribTracker/ContribTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContribTrackerApp.swift
+//  ContribTracker
+//
+//  Created by Wayne Dahlberg on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ContribTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
